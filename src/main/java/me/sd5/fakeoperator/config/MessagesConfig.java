65c505b5136354273294458208115ec92091ae6a
Timo <timo.braendlin@googlemail.com>
2014-10-12 20:16:48 +0200

    Added op and deop messages

--- src/main/java/me/sd5/fakeoperator/config/MessagesConfig.java
+++ src/main/java/me/sd5/fakeoperator/config/MessagesConfig.java
@@ -4,8 +4,19 @@ import org.bukkit.plugin.java.JavaPlugin;
 
 public class MessagesConfig extends Config {
 
+	private final String opPath = "messages.fake-op";
+	private final String deopPath = "messages.fake-deop";
+	
 	public MessagesConfig(JavaPlugin plugin) {
 		super(plugin, "messages.yml");
+	}
+	
+	public String getFakeOpMessage() {
+		return get().getString(opPath);
+	}
+	
+	public String getFakeDeopMessage() {
+		return get().getString(deopPath);
 	}
 	
 }
