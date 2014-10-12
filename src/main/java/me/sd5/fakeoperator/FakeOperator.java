50a812f0afc9505b8c3abbe0fda7190df3379e7e
Timo <timo.braendlin@googlemail.com>
2014-10-12 18:46:07 +0200

    Set up the project

---
+++ src/main/java/me/sd5/fakeoperator/FakeOperator.java
@@ -0,0 +1,17 @@
+package me.sd5.fakeoperator;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class FakeOperator extends JavaPlugin {
+
+	@Override
+	public void onEnable() {
+		
+	}
+	
+	@Override
+	public void onDisable() {
+		
+	}
+	
+}
